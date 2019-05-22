9e868b90e7805d1bcb48c75bc37176df78687d82
SKPnation <ayomideseaz@gmail.com>
2019-05-22 19:43:42 +0000

    admin can now upload, update and delete a food item in a specific menu category

--- app/src/main/java/com/example/ayomide/chowadminapp/FoodList.java
+++ app/src/main/java/com/example/ayomide/chowadminapp/FoodList.java
@@ -7,14 +7,17 @@ import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -46,9 +49,11 @@ public class FoodList extends AppCompatActivity {
 
     FloatingActionButton fab;
 
+    RelativeLayout rootLayout;
+
     //Firebase
     FirebaseDatabase db;
-    DatabaseReference food;
+    DatabaseReference foodlist;
     FirebaseStorage storage;
     StorageReference storageReference;
 
@@ -70,7 +75,7 @@ public class FoodList extends AppCompatActivity {
         setContentView( R.layout.activity_food_list );
 
         db = FirebaseDatabase.getInstance();
-        food = db.getReference("Food");
+        foodlist = db.getReference("Food");
         storage = FirebaseStorage.getInstance();
         storageReference = storage.getReference();
 
@@ -78,6 +83,8 @@ public class FoodList extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+
+        rootLayout = findViewById(R.id.rootLayout);
 
         fab = (FloatingActionButton) findViewById( R.id.fab );
         fab.setOnClickListener( new View.OnClickListener() {
@@ -100,7 +107,7 @@ public class FoodList extends AppCompatActivity {
                 Food.class,
                 R.layout.food_item,
                 FoodViewHolder.class,
-                food.orderByChild("menuId").equalTo(categoryId)) {
+                foodlist.orderByChild("menuId").equalTo(categoryId)) {
             @Override
             protected void populateViewHolder(FoodViewHolder viewHolder, Food model, int position) {
                 viewHolder.food_name.setText(model.getName());
@@ -125,6 +132,7 @@ public class FoodList extends AppCompatActivity {
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(FoodList.this);
         alertDialog.setTitle("Add new Food");
         alertDialog.setMessage("Put in full information");
+        alertDialog.setIcon(R.drawable.ic_shopping_cart_black_24dp);
 
         LayoutInflater inflater = this.getLayoutInflater();
         View add_menu_layout = inflater.inflate(R.layout.add_new_food_layout, null);
@@ -152,7 +160,6 @@ public class FoodList extends AppCompatActivity {
         });
 
         alertDialog.setView(add_menu_layout);
-        alertDialog.setIcon(R.drawable.ic_shopping_cart_black_24dp);
 
         //set button
         alertDialog.setPositiveButton( "YES", new DialogInterface.OnClickListener() {
@@ -163,7 +170,7 @@ public class FoodList extends AppCompatActivity {
                 //Here create new category
                 if(newFood != null)
                 {
-                    food.push().setValue(newFood);
+                    foodlist.push().setValue(newFood);
                 }
             }
         });
@@ -201,7 +208,7 @@ public class FoodList extends AppCompatActivity {
                         @Override
                         public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
                             mDialog.dismiss();
-                            Toast.makeText(FoodList.this, "Uploaded!!!", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(FoodList.this, "Uploaded successfully", Toast.LENGTH_SHORT).show();
                             imageFolder.getDownloadUrl().addOnSuccessListener( new OnSuccessListener<Uri>() {
                                 @Override
                                 public void onSuccess(Uri uri) {
@@ -234,7 +241,8 @@ public class FoodList extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
         super.onActivityResult( requestCode, resultCode, data );
 
         if(requestCode == Common.IMAGE_REQUEST && resultCode == RESULT_OK
@@ -244,4 +252,136 @@ public class FoodList extends AppCompatActivity {
             btnSelect.setText("Image Selected");
         }
     }
+
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item)
+    {
+        if(item.getTitle().equals(Common.UPDATE))
+        {
+            showUpdateFoodDialog(adapter.getRef(item.getOrder()).getKey(), adapter.getItem(item.getOrder()));
+        }
+        else if(item.getTitle().equals(Common.DELETE))
+        {
+            deleteCategory(adapter.getRef(item.getOrder()).getKey());
+        }
+        return super.onContextItemSelected( item );
+    }
+
+    private void deleteCategory(String key)
+    {
+        foodlist.child(key).removeValue();
+        Toast.makeText(FoodList.this, "Item deleted!!!", Toast.LENGTH_SHORT ).show();
+    }
+
+    private void changeImage(final Food item)
+    {
+        if(saveUri != null)
+        {
+            final ProgressDialog mDialog = new ProgressDialog(this);
+            mDialog.setMessage("Uploading...");
+            mDialog.show();
+
+            String imageName = UUID.randomUUID().toString();
+            final StorageReference imageFolder = storageReference.child("images/"+imageName);
+            imageFolder.putFile(saveUri)
+                    .addOnSuccessListener( new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            mDialog.dismiss();
+                            Toast.makeText(FoodList.this, "Uploaded!!!", Toast.LENGTH_SHORT).show();
+                            imageFolder.getDownloadUrl().addOnSuccessListener( new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    item.setImage(uri.toString());
+                                }
+                            });
+                        }
+                    })
+                    .addOnFailureListener( new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            mDialog.dismiss();
+                            Toast.makeText( FoodList.this, ""+e.getMessage(), Toast.LENGTH_LONG ).show();
+                        }
+                    })
+                    .addOnProgressListener( new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                            mDialog.setMessage( "Uploaded "+progress+"%" );
+                        }
+                    });
+        }
+    }
+
+    private void showUpdateFoodDialog(final String key, final Food item)
+    {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(FoodList.this);
+        alertDialog.setTitle("Edit Food");
+        alertDialog.setMessage("Put in full information");
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        View add_menu_layout = inflater.inflate(R.layout.add_new_food_layout, null);
+
+        edtName = add_menu_layout.findViewById(R.id.etName);
+        edtDescription = add_menu_layout.findViewById(R.id.etDesc);
+        edtPrice = add_menu_layout.findViewById(R.id.etPrice);
+        edtDiscount = add_menu_layout.findViewById(R.id.etDiscount);
+
+        //set default value for view
+        edtName.setText(item.getName());
+        edtDescription.setText(item.getDescription());
+        edtPrice.setText(item.getPrice());
+        edtDiscount.setText(item.getDiscount());
+
+        btnSelect = add_menu_layout.findViewById(R.id.btnSelect);
+        btnUpload = add_menu_layout.findViewById(R.id.btnUpload);
+
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseImage(); //let user select image from gallery and save Uri of this image
+            }
+        });
+
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeImage(item);
+            }
+        });
+
+        alertDialog.setView(add_menu_layout);
+        alertDialog.setIcon(R.drawable.ic_shopping_cart_black_24dp);
+
+        //set button
+        alertDialog.setPositiveButton( "YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+
+                    //update information
+                    item.setName(edtName.getText().toString());
+                    item.setDescription(edtDescription.getText().toString());
+                    item.setPrice(edtPrice.getText().toString());
+                    item.setDiscount(edtDiscount.getText().toString());
+
+                    foodlist.child(key).setValue(item);
+
+                    Snackbar.make(rootLayout, "Food " +item.getName()+ " was edited", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
+        alertDialog.setNegativeButton( "NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        alertDialog.show();
+    }
+
+
 }
